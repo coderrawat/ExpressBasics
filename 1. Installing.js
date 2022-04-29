d4d5d9fa48feb16a54a8f01d4951c6407bda5193
coderrawat <coderrawat@gmail.com>
2022-04-29 11:45:55 -0400

    Added installing express

---
+++ 1. Installing.js
@@ -0,0 +1,19 @@
+/* 
+*-----------------------------------*
+
+Prerequisite: 
+1. Node.js already installed
+2. Create working directory
+
+*-----------------------------------*
+
+Step 1: Create a package.json file
+$ npm init -y
+
+*-----------------------------------*
+
+Step 2: Install Express
+npm install express
+
+*-----------------------------------*
+*/
