8fe5823b6f214038237b1f2042cadd938aa03b05
coderrawat <coderrawat@gmail.com>
2022-04-30 15:59:13 -0400

    Added AppGenerator.js file

---
+++ 3.AppGenerator.js
@@ -0,0 +1,18 @@
+/* 
+
+Generate App structure for Node.js 8.2.0 and up
+$ npx express-generator
+
+Generate App structure for previous 
+$ npm install -g express-generator
+$ express
+
+Create express app called demoapp with hbs view engine
+$ express --view=hbs demoapp
+
+Install dependencies once app structure generated
+$ npm install
+
+Note: App Structure can be generated manaully as per need.
+
+*/
